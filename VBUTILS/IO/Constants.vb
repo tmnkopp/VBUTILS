d8e23213ec7b9dc44ef96cb5459559ceb06789e9
TimKopp <timkopp@gmail.com>
2019-08-27 18:43:37 -0400

    add io folder

---
+++ VBUTILS/IO/Constants.vb
@@ -0,0 +1,10 @@
+﻿Public Class Constants
+    Public Shared ReadOnly HR As New String("-"c, 25)
+    Public Shared ReadOnly FolderRoot As New String($"D:\dev\CyberScope\CyberScopeBranch\CSwebdev")
+    Public Shared ReadOnly FolderCodeArchive As New String($"{Constants.FolderRoot}\code\")
+    Public Shared ReadOnly FolderFF2018 As New String($"{Constants.FolderRoot}\code\CyberScope\FismaForms\2018\")
+    Public Shared ReadOnly FolderDB As New String($"{Constants.FolderRoot}\database\")
+    Public Shared ReadOnly FolderDBArchive As New String($"{Constants.FolderDB}\Archive\")
+    Public Shared ReadOnly FolderDBSprocs As New String($"{Constants.FolderDB}\Sprocs\")
+    Public Shared ReadOnly FolderDBViews As New String($"{Constants.FolderDB}\Views\")
+End Class
---
+++ VBUTILS/IO/ICodeFileProfile.vb
@@ -0,0 +1,54 @@
+﻿Imports VBUTILS
+
+Public Interface ICodeParseProfile
+    Property ID As String
+    Property Path As String
+    Property SearchOption As String
+End Interface
+
+Public Class CodeParseProfile
+    Implements ICodeParseProfile
+
+    Private _id As String
+    Private _path As String
+    Private _searchOption As String
+    Private _snippet As String
+    Public Sub New()
+    End Sub
+    Public Sub New(SearchOption As String)
+        Me.SearchOption = SearchOption
+    End Sub
+    Public Property ID As String Implements ICodeParseProfile.ID
+        Get
+            Return _id
+        End Get
+        Set(value As String)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Path As String Implements ICodeParseProfile.Path
+        Get
+            Return _path
+        End Get
+        Set(value As String)
+            _path = value
+        End Set
+    End Property
+    Public Property SearchOption As String Implements ICodeParseProfile.SearchOption
+        Get
+            Return _searchOption
+        End Get
+        Set(value As String)
+            _searchOption = value
+        End Set
+    End Property
+    Public Property Snippet As String
+        Get
+            Return _snippet
+        End Get
+        Set(value As String)
+            _snippet = value
+        End Set
+    End Property
+End Class
