e5313b3f2fe565f84d52fe23af441d0d339e0451
Tim Kopp <tim.kopp@cyber-balance.com>
2019-08-30 14:58:23 -0400

    added new lookup for lists of values

--- VBUTILS/Data/DBUtils.vb
+++ VBUTILS/Data/DBUtils.vb
@@ -20,5 +20,21 @@ Namespace Data
             oConn.Dispose()
             Return sRetSB.ToString()
         End Function
+        Public Function dbLookUpList(sSql As String) As List(Of String)
+            Dim sConn As String = ConfigurationManager.ConnectionStrings("Cyberscope123").ToString()
+            Dim oConn As SqlConnection = New SqlConnection(sConn)
+            Dim sRetList As New List(Of String)
+            oConn.Open()
+            Using oCmd As SqlCommand = New SqlCommand(sSql, oConn)
+                Using oRead As SqlDataReader = oCmd.ExecuteReader()
+                    While oRead.Read
+                        sRetList.Add(oRead.GetValue(0).ToString)
+                    End While
+                End Using
+            End Using
+            oConn.Close()
+            oConn.Dispose()
+            Return sRetList
+        End Function
     End Class
 End Namespace
